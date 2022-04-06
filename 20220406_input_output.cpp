08309bc78c8080948e6847df18c87153e74beaa2
Valenangellina <103085800+Valenangellina@users.noreply.github.com>
2022-04-05 20:28:39 -0700

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,9 @@
+//header 
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+string nama; 
+cout<<"Masukkan Nama:";cin>>nama;
+cout<<"Nama Anda:"<<nama;
+}//end of the function
+
