e7d6cf718661282103b8db911a7828fc20b4daf2
Valenangellina <103085800+Valenangellina@users.noreply.github.com>
2022-04-05 20:10:05 -0700

    Create 20220406_variabel.cpp

---
+++ 20220406_variabel.cpp
@@ -0,0 +1,11 @@
+//header 
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+float angka;
+string nama; //deklarasi variabel
+nama = "Valen Angellina Jocelyn"; //masukkan value kedalam variabel
+angka = 10.21+3.14;
+cout<<nama<<endl; //menampilkan variabel 
+cout<<angka<<endl;
+}//end of the function
