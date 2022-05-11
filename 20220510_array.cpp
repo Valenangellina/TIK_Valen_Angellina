5c00297fd292bcdbe3cc478ddaa16d6e8326ba6e
Valenangellina <103085800+Valenangellina@users.noreply.github.com>
2022-05-10 19:02:22 -0700

    Create 20220510_array.cpp

---
+++ 20220510_array.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nama[] = {
+        "David", 
+        "Kabila", 
+        "Bryan", 
+        "Sydney",
+        "Gracia",
+        "Keysie",
+        "Yoel",
+        
+    };
+    for (int i = 0; i<7 ; i ++) {
+    cout<<nama[i]<<endl;
+    }
+    
+    return 0;
+    
+}
