37d92c81c188da4dc40001e37d9397d9e9927e19
Valenangellina <103085800+Valenangellina@users.noreply.github.com>
2022-05-10 19:55:53 -0700

    Create 202200511_arrayangka.cpp

---
+++ 202200511_arrayangka.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int angka[] = {
+     
+    };
+    for (int i= 0; i<5;i ++){
+        cout<<"Masukkan Angka =";cin>>angka[i];
+    }
+    for (int a=0; a<5;a ++){
+        cout<<"Angka ke -"<<a<<"="<<angka[a]*angka[a]<<endl;
+    }
+return 0;
+    
+}
